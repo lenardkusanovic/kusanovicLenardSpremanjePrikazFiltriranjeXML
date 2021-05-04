4cf7497c5caba47cf4d25597575d8819b6de8c90
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2021-05-04 13:58:07 +0200

    Add files via upload

---
+++ kusanovicLenardSpremanjePrikazFiltriranjeXML/kusanovicLenardSpremanjePrikazFiltriranjeXML/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace kusanovicLenardSpremanjePrikazFiltriranjeXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnos = new System.Windows.Forms.Button();
+            this.btnPrikaz = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUnos
+            // 
+            this.btnUnos.Location = new System.Drawing.Point(60, 229);
+            this.btnUnos.Name = "btnUnos";
+            this.btnUnos.Size = new System.Drawing.Size(203, 38);
+            this.btnUnos.TabIndex = 0;
+            this.btnUnos.Text = "Unos";
+            this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPrikaz
+            // 
+            this.btnPrikaz.Location = new System.Drawing.Point(60, 273);
+            this.btnPrikaz.Name = "btnPrikaz";
+            this.btnPrikaz.Size = new System.Drawing.Size(203, 40);
+            this.btnPrikaz.TabIndex = 1;
+            this.btnPrikaz.Text = "Prikaz";
+            this.btnPrikaz.UseVisualStyleBackColor = true;
+            this.btnPrikaz.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kusanovicLenardSpremanjePrikazFiltriranjeXML.Properties.Resources.xmlTransformer_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 201);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 386);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPrikaz);
+            this.Controls.Add(this.btnUnos);
+            this.Name = "Form1";
+            this.Text = "XML Transformer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnos;
+        private System.Windows.Forms.Button btnPrikaz;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
